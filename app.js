31a1497b3cda64893f98db2707037ff8a1030e17
Taras-js <78747117+Taras-js@users.noreply.github.com>
2021-03-11 13:50:25 +0400

    Create website-project-adaptive-on-html5-in-css3

---
+++ app.js
@@ -0,0 +1,27 @@
+
+// const heading = document.getElementById('gad')
+// console.dir(heading.textContent)
+
+
+
+// setTimeout(() => {
+//     addStyleAll(heading)
+// }, 4500);
+
+// function addStyleAll(node) {
+//     node.textContent = 'red'
+//     node.style.color = 'red'
+//     node.style.textAlign = 'center'
+// }
+// const heading2 = document.getElementById('god')
+// console.dir(heading2.textContent)
+
+
+
+// setTimeout(() => {
+//     addStyleAll2(heading2)
+// }, 4500);
+
+// function addStyleAll2(node) {
+//     node.style.color = 'red'
+// }
